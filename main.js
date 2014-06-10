0fdcc4925531020a90d08c99c1dde29d146dc240
Jason Madigan <jason.madigan@feedhenry.com>
2014-06-10 17:57:17 +0100

    Refactoring service

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-
 /*
  Inserts an object (document) into a collection in MongoDB
  @param params.insert an object to insert into your database
  @param params.collection the collection to insert it into
  */
-exports.mongodb = function(params, cb){
+exports.mongodb = function(params, cb) {
   var MongoClient = require('mongodb').MongoClient,
-          format = require('util').format,
-          user = process.env.MONGODB_USER,
-          password = process.env.MONGODB_PASSWORD,
-          upString = (typeof user === 'string' && typeof password === 'string') ? user + ":" + password : "",
-          database = process.env.MONGODB_DATABASE,
-          host = process.env.MONGODB_HOST;
+    format = require('util').format,
+    user = process.env.MONGODB_USER,
+    password = process.env.MONGODB_PASSWORD,
+    upString = (typeof user === 'string' && typeof password === 'string') ? user + ":" + password : "",
+    database = process.env.MONGODB_DATABASE,
+    host = process.env.MONGODB_HOST;
 
   MongoClient.connect('mongodb://' + upString + '@' + host + '/' + database, function(err, db) {
-    if(err) return cb(err);
+    if (err) return cb(err);
 
     var collection = db.collection(params.collection);
     collection.insert(params.insert, function(err, docs) {
@@ -22,5 +21,4 @@ exports.mongodb = function(params, cb){
       return cb(null, docs);
     });
   })
-
 };
