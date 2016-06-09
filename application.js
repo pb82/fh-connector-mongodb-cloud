a2f223b3811bb39197a883ccaa6465661fd1c202
Leonardo Rossetti <lrossett@redhat.com>
2016-06-09 20:34:27 -0300

    Update mongodb driver (>= 2.1.0) + test node 4.x support
    https://issues.jboss.org/browse/RHMAP-6233

--- application.js
+++ application.js
@@ -29,6 +29,7 @@ app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
